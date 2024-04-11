c407bd80747817aee1182ede6415fb6b058b1280
Shin Seung Ho <tlstmdgh8620@gmail.com>
2024-04-11 17:09:55 +0900

    Feat: BottomBar 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import TopBar from '@/components/common/header';
+import BottomBar from '@/components/common/bottom-bar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <TopBar />
         {children}
+        <BottomBar />
       </body>
     </html>
   );
