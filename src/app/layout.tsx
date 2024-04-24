f0ccf6eed526220d05cad0f6a52cbb4374d87a8b
Shin Seung Ho <tlstmdgh8620@gmail.com>
2024-04-24 11:42:19 +0900

    Update: 헤더 변경(수정예정)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import TopBar from '@/components/common/header';
+// import TopBar from '@/components/common/header';
 import BottomBar from '@/components/common/bottom-bar';
 import { cn } from '@/lib/utils';
+import NavBar from '@/components/common/headertemp';
 
 const Pretendard = localFont({
   src: '../../public/font/PretendardVariable.woff2',
@@ -21,8 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={cn(Pretendard.className)}>
-        <TopBar />
+      <body
+        className={cn(Pretendard.className, 'max-w-screen overflow-x-hidden')}
+      >
+        {/* <TopBar /> */}
+        <NavBar />
         {children}
         <BottomBar />
       </body>
